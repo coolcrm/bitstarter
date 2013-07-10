dac14371b4984092580cf727d71597656b43c371
vk <vk@D430.(none)>
2013-07-11 00:27:46 +0400

    Commit

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+var fs = require('fs);
+fs.readFileSync("index.html");
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World2s!');
 });
 
 var port = process.env.PORT || 5000;
