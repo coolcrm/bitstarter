f3a0e0282041e356acb33fc838e37d83e6e2b7e8
vk <vk@D430.(none)>
2013-07-11 00:33:55 +0400

    Web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
+require('buffer');
 var fs = require('fs);
-fs.readFileSync("index.html");
+buf = new Buffer(300);
+buf = fs.readFileSync("index.html");
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2s!');
+//  response.send('Hello World2s!');
+response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
